8b04a807e970dead0fcd234379fcaf614cb03686
Sruthy24 <sruthysmnglm@gmail.com>
2025-07-16 10:32:23 +0530

    Add files via upload

---
+++ Positivenegative.java
@@ -0,0 +1,22 @@
+package lab;
+
+public class Positivenegative {
+	public static void main(String []arg) {
+		int number =10;
+		if(number>0)
+		{
+			System.out.println("positive");
+		}
+		
+			else if(number<0)
+			{
+				System.out.println("negative");
+			}
+			else {
+				System.out.println("zero");
+			
+		}
+		
+	}
+
+}
