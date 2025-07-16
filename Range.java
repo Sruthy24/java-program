aeff8c02f6fb0e80ed62a5309eaefceedad387e3
Sruthy24 <sruthysmnglm@gmail.com>
2025-07-16 10:08:24 +0530

    Add files via upload

---
+++ Range.java
@@ -0,0 +1,19 @@
+package lab;
+
+import java.util.Scanner;
+
+public class Range {
+	public static void main(String [] arg) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a range");
+		int number = sc.nextInt();
+		System.out.println("the numbers are");
+		for(int i=0;i<=number;i++)
+		System.out.println(i);
+	}
+
+
+}
+	
+
+
