6854bb335ad70ab0357b182e9beae911022727de
Sruthy24 <sruthysmnglm@gmail.com>
2025-07-23 09:51:56 +0530

    Add files via upload

---
+++ week.java
@@ -0,0 +1,35 @@
+package lab;
+import java.util.Scanner;
+public class week {
+	public static void main(String []args) {
+		Scanner SC=new Scanner(System.in);
+		System.out.println("enter the number");
+		 int days =SC.nextInt();
+ switch(days) {
+ case 1:
+	 System.out.println("monday");
+	 break;
+ case 2:
+	 System.out.println("tuesday");
+	 break;
+ case 3:
+	 System.out.println("wednesday");
+	 break;
+ case 4:
+	 System.out.println("thursday");
+	 break;
+ case 5:
+	 System.out.println("friday");
+	 break;
+ case 6:
+	 System.out.println("saturday");
+	 break;
+ case 7:
+	 System.out.println("sunday");
+	 break;
+default:	 	 	 	 	 	 
+	 System.out.println("error");
+     break;
+ }
+	}
+}
