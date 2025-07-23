866e7e29af5e5d7fd9c017238b827359968d73ed
Sruthy24 <sruthysmnglm@gmail.com>
2025-07-23 10:03:45 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,14 @@
+package lab;
+import java.util.Scanner;
+public class odd {
+public static void main(String[]arg) {
+	for(int i=0;i<=50;i++) {
+	 if(i%2 ==0) {
+	 continue;
+	 }
+	 else {
+		 System.out.println(+i);
+	 }
+}
+}
+}
